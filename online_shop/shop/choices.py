0e1b517d083e615b6a6c875c13347ac3f3b4a3a7
maka <maka.saparashvili@gmail.com>
2025-10-30 19:37:59 +0400

    updated choices with delivery statuses

---
+++ online_shop/shop/choices.py
@@ -0,0 +1,29 @@
+COLOR_CHOICES = [
+    ("white", "White"),
+    ("black", "Black"),
+    ("brown", "Brown"),
+    ("gray", "Gray"),
+    ("beige", "Beige"),
+    ("red", "Red"),
+    ("green", "Green"),
+    ("golden", "Golden"),
+    ("blue", "Blue"),
+]
+
+MATERIAL_CHOICES = [
+    ("wood", "Wood"),
+    ("metal", "Metal"),
+    ("glass", "Glass"),
+    ("leather", "Leather"),
+    ("textile", "Textile"),
+    ("plastic", "Plastic"),
+    ("polyester","Polyester")
+]
+
+STATUS_CHOICES = [
+    ("pending", "Pending"),
+    ("processing", "Processing"),
+    ("shipped", "Shipped"),
+    ("delivered", "Delivered"),
+    ("cancelled", "Cancelled"),
+]
